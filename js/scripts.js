5a970a078cd5557833428ec2d41a189bac65e1c4
TomaszAndrzejewski <tomasz.andrzejewski.ux@wp.pl>
2018-02-17 21:42:36 +0100

    Zmiana sposobu animacji i RWD

--- js/scripts.js
+++ js/scripts.js
@@ -94,24 +94,37 @@ function scrollFunction1() {
 }
 
 
+
 /*ANIMACJA O NAS*/
 
-var bg = document.querySelector("#about");
 
-bg.addEventListener("mouseenter", showTextContainer);
+/**
+ * jQuery-viewport-checker - v1.8.8 - 2017-09-25
+ * https://github.com/dirkgroenen/jQuery-viewport-checker
+ *
+ * Copyright (c) 2017 Dirk Groenen
+ * Licensed MIT <https://github.com/dirkgroenen/jQuery-viewport-checker/blob/master/LICENSE>
+ */
 
-function showTextContainer() {
-	var textContainer = document.querySelector(".animatedLeft");
-	var textContainer2 = document.querySelector(".animatedRight");
-	textContainer.classList.add("text-visible");
-	textContainer2.classList.add("text-visible");
-	bg.removeEventListener("mouseenter", showTextContainer);
-}
+!function(a){a.fn.viewportChecker=function(b){var c={classToAdd:"visible",classToRemove:"invisible",classToAddForFullView:"full-visible",removeClassAfterAnimation:!1,offset:100,repeat:!1,invertBottomOffset:!0,callbackFunction:function(a,b){},scrollHorizontal:!1,scrollBox:window};a.extend(c,b);var d=this,e={height:a(c.scrollBox).height(),width:a(c.scrollBox).width()};return this.checkElements=function(){var b,f;c.scrollHorizontal?(b=Math.max(a("html").scrollLeft(),a("body").scrollLeft(),a(window).scrollLeft()),f=b+e.width):(b=Math.max(a("html").scrollTop(),a("body").scrollTop(),a(window).scrollTop()),f=b+e.height),d.each(function(){var d=a(this),g={},h={};if(d.data("vp-add-class")&&(h.classToAdd=d.data("vp-add-class")),d.data("vp-remove-class")&&(h.classToRemove=d.data("vp-remove-class")),d.data("vp-add-class-full-view")&&(h.classToAddForFullView=d.data("vp-add-class-full-view")),d.data("vp-keep-add-class")&&(h.removeClassAfterAnimation=d.data("vp-remove-after-animation")),d.data("vp-offset")&&(h.offset=d.data("vp-offset")),d.data("vp-repeat")&&(h.repeat=d.data("vp-repeat")),d.data("vp-scrollHorizontal")&&(h.scrollHorizontal=d.data("vp-scrollHorizontal")),d.data("vp-invertBottomOffset")&&(h.scrollHorizontal=d.data("vp-invertBottomOffset")),a.extend(g,c),a.extend(g,h),!d.data("vp-animated")||g.repeat){String(g.offset).indexOf("%")>0&&(g.offset=parseInt(g.offset)/100*e.height);var i=g.scrollHorizontal?d.offset().left:d.offset().top,j=g.scrollHorizontal?i+d.width():i+d.height(),k=Math.round(i)+g.offset,l=g.scrollHorizontal?k+d.width():k+d.height();g.invertBottomOffset&&(l-=2*g.offset),k<f&&l>b?(d.removeClass(g.classToRemove),d.addClass(g.classToAdd),g.callbackFunction(d,"add"),j<=f&&i>=b?d.addClass(g.classToAddForFullView):d.removeClass(g.classToAddForFullView),d.data("vp-animated",!0),g.removeClassAfterAnimation&&d.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",function(){d.removeClass(g.classToAdd)})):d.hasClass(g.classToAdd)&&g.repeat&&(d.removeClass(g.classToAdd+" "+g.classToAddForFullView),g.callbackFunction(d,"remove"),d.data("vp-animated",!1))}})},("ontouchstart"in window||"onmsgesturechange"in window)&&a(document).bind("touchmove MSPointerMove pointermove",this.checkElements),a(c.scrollBox).bind("load scroll",this.checkElements),a(window).resize(function(b){e={height:a(c.scrollBox).height(),width:a(c.scrollBox).width()},d.checkElements()}),this.checkElements(),this}}(jQuery);
+//# sourceMappingURL=jquery.viewportchecker.min.js.map
+
+$(document).ready(function(){
+ 
+		$('*[data-animate]').addClass('hide').each(function(){
+      $(this).viewportChecker({
+        classToAdd: 'show animated ' + $(this).data('animate'),
+        classToRemove: 'hide',
+        offset: '10%'
+      });
+    });
+ 
+	});
 
 
 /* ANIMACJA OFERTA */
 
-
+/*
 var bg2 = document.querySelector("#oferta");
 
 bg2.addEventListener("mouseenter", showTextContainer3);
@@ -126,9 +139,9 @@ function showTextContainer3() {
 	bg2.removeEventListener("mouseenter", showTextContainer3);
 }
 
-
+*/
 /* ANIMACJA VIDEO */
-
+/*
 
 var bg3 = document.querySelector("#video");
 
@@ -150,4 +163,4 @@ function showTextContainer4() {
 	videoContainer6.classList.add("video-visible");
 
 	bg3.removeEventListener("mouseenter", showTextContainer4);
-}
+}*/
